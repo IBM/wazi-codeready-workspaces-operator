7c836ca50357a95c010d4f88c29d39187a878466
nickboldt <nboldt@redhat.com>
2020-06-05 16:46:14 -0400

    Revert "CRW-942 no longer need to toggle between rhcc and quay operator image names"
    This reverts commit 3aaefb831f5454adacd1bbec40dc6b8e92a80351.

--- build/scripts/buildDigestMapAlternateURLs.sh
+++ build/scripts/buildDigestMapAlternateURLs.sh
@@ -4,6 +4,8 @@
             `# for plugin & devfile registries, use internal Brew versions` \
             -e "s|registry.redhat.io/codeready-workspaces/(pluginregistry-rhel8:.+)|registry-proxy.engineering.redhat.com/rh-osbs/codeready-workspaces-\\1|g" \
             -e "s|registry.redhat.io/codeready-workspaces/(devfileregistry-rhel8:.+)|registry-proxy.engineering.redhat.com/rh-osbs/codeready-workspaces-\\1|g" \
+            `# for operator, replace internal container name with quay name` \
+            -e "s|crw-2-rhel8-operator|operator-rhel8|g" \
             `# in all other cases (including operator) use published quay images to compute digests` \
             -e "s|registry.redhat.io/codeready-workspaces/(.+)|quay.io/crw/\\1|g" \
             > ${tmpfile}
