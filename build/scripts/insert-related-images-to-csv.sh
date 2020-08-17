3109d67ca36dcb16be3aed8f02ddf9f6fd8eb523
nickboldt <nboldt@redhat.com>
2020-08-17 10:54:52 -0400

    CRW-1162 disable switch from external to internal refs
    Change-Id: Ifae1e861d58cae36f9857c093abb2620e386562c
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- build/scripts/insert-related-images-to-csv.sh
+++ build/scripts/insert-related-images-to-csv.sh
@@ -67,11 +67,12 @@ for updateVal in "${CONTAINERS[@]}"; do
     > ${CSVFILE}.2; mv ${CSVFILE}.2 ${CSVFILE}
 done
 
-# replace external crw refs with internal ones
-sed -r -i $CSVFILE \
-  -e "s@registry.redhat.io/codeready-workspaces/@registry-proxy.engineering.redhat.com/rh-osbs/codeready-workspaces-@g#" \
-  -e "s@registry-proxy.engineering.redhat.com/rh-osbs/codeready-workspaces-crw-2-rhel8-operator@registry-proxy.engineering.redhat.com/rh-osbs/codeready-workspaces-operator@g" \
-  -e "s@registry.access.redhat.com/ubi8-minimal@registry.redhat.io/ubi8-minimal@g"
+# TODO: CRW-1162 disable this: replace external crw refs with internal ones
+#sed -r -i $CSVFILE \
+#  -e "s@registry.redhat.io/codeready-workspaces/@registry-proxy.engineering.redhat.com/rh-osbs/codeready-workspaces-@g#" \
+#  -e "s@registry-proxy.engineering.redhat.com/rh-osbs/codeready-workspaces-crw-2-rhel8-operator@registry-proxy.engineering.redhat.com/rh-osbs/codeready-workspaces-operator@g" \
+#  -e "s@registry.access.redhat.com/ubi8-minimal@registry.redhat.io/ubi8-minimal@g"
+
 
 # echo list of RELATED_IMAGE_ entries after adding them above
 # cat $CSVFILE | grep RELATED_IMAGE_ -A1
