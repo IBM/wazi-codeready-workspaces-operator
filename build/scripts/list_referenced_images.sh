457722e6da1063445869910a44f8f585d6883801
nickboldt <nboldt@redhat.com>
2020-08-27 12:55:25 -0400

    list_referenced_images.sh doesn't work on rhel7 nodes, so fluff it up so it works on older rhel; also dedupe
    Change-Id: Iff7a99c7b68c236afca92d4fd745c134493a9707
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- build/scripts/list_referenced_images.sh
+++ build/scripts/list_referenced_images.sh
@@ -10,9 +10,28 @@
 # List all images referenced in csv.yaml files
 #
 
-# Usage (OCP 4.5-): list_referenced_images.sh controller-manifests/v2.2.0/
+# Usage (OCP 4.5-): list_referenced_images.sh controller-manifests/v2.4.0/
 # Usage (OCP 4.6+): list_referenced_images.sh manifests/ 
 set -e
-readarray -d '' metas < <(find "$1" -name '*csv.yaml' -print0)
-yq -r '..|.image?' "${metas[@]}" | grep -E "quay|registry" | sort | uniq
-yq -r '..|.value?' "${metas[@]}" | grep -E "quay|registry" | sort | uniq
+
+# simpler way (includes duplicates) + doesn't work on rhel7 (readarray -d)
+#readarray -d '' metas < <(find "$1" -name '*csv.yaml' -print0)
+#yq -r '..|.image?' "${metas[@]}" | grep -E "quay|registry" | sort | uniq
+#yq -r '..|.value?' "${metas[@]}" | grep -E "quay|registry" | sort | uniq
+
+CONTAINERS=""
+
+while IFS= read -r -d '' file; do
+  CONTAINERS="${CONTAINERS} $(yq -r '..|.image?' "${file}" | grep -E "quay|registry" | sort | uniq)"
+  CONTAINERS="${CONTAINERS} $(yq -r '..|.value?' "${file}" | grep -E "quay|registry" | sort | uniq)"
+done < <(find "$1" -name '*csv.yaml' -print0)
+
+CONTAINERS_UNIQ=()
+# shellcheck disable=SC2199
+for c in $CONTAINERS; do if [[ ! "${CONTAINERS_UNIQ[@]}" =~ ${c} ]]; then CONTAINERS_UNIQ+=("$c"); fi; done
+# shellcheck disable=SC2207
+IFS=$'\n' CONTAINERS=($(sort <<<"${CONTAINERS_UNIQ[*]}")); unset IFS
+
+for c in "${CONTAINERS[@]}"; do
+  echo "$c"
+done
